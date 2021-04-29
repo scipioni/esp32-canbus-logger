150c6e390e583852840db712c101c5da0fbb19c1
Stefano Scipioni <scipio.it@gmail.com>
2021-04-29 16:11:25 +0200

    log to file

--- src/main.cpp
+++ src/main.cpp
@@ -1,232 +1,34 @@
 #include <Arduino.h>
 #include "led.h"
-
-// sd card
-#include "FS.h"
-#include "SD.h"
-#include "SPI.h"
-
-// canbus
+#include "sd.h"
 #include <ESP32CAN.h>
 #include <CAN_config.h>
 
-CAN_device_t CAN_cfg;             // CAN Config
-unsigned long previousMillis = 0; // will store last time a CAN Message was send
-//const int interval = 1000;          // interval at which send CAN Messages (milliseconds)
+CAN_device_t CAN_cfg;         // CAN Config
 const int rx_queue_size = 10; // Receive Queue size
 
+File file_log;
+//char message[128];
+
 /*
+sd card pinout:
 CS GPIO2
 SCK GPIO18
 MISO GPIO19
 MOSI GPIO23
 */
 
-void listDir(fs::FS &fs, const char *dirname, uint8_t levels)
-{
-  Serial.printf("Listing directory: %s\n", dirname);
-
-  File root = fs.open(dirname);
-  if (!root)
-  {
-    Serial.println("Failed to open directory");
-    return;
-  }
-  if (!root.isDirectory())
-  {
-    Serial.println("Not a directory");
-    return;
-  }
-
-  File file = root.openNextFile();
-  while (file)
-  {
-    if (file.isDirectory())
-    {
-      Serial.print("  DIR : ");
-      Serial.println(file.name());
-      if (levels)
-      {
-        listDir(fs, file.name(), levels - 1);
-      }
-    }
-    else
-    {
-      Serial.print("  FILE: ");
-      Serial.print(file.name());
-      Serial.print("  SIZE: ");
-      Serial.println(file.size());
-    }
-    file = root.openNextFile();
-  }
-}
-void createDir(fs::FS &fs, const char *path)
-{
-  Serial.printf("Creating Dir: %s\n", path);
-  if (fs.mkdir(path))
-  {
-    Serial.println("Dir created");
-  }
-  else
-  {
-    Serial.println("mkdir failed");
-  }
-}
-
-void removeDir(fs::FS &fs, const char *path)
-{
-  Serial.printf("Removing Dir: %s\n", path);
-  if (fs.rmdir(path))
-  {
-    Serial.println("Dir removed");
-  }
-  else
-  {
-    Serial.println("rmdir failed");
-  }
-}
-
-void readFile(fs::FS &fs, const char *path)
-{
-  Serial.printf("Reading file: %s\n", path);
-
-  File file = fs.open(path);
-  if (!file)
-  {
-    Serial.println("Failed to open file for reading");
-    return;
-  }
-
-  Serial.print("Read from file: ");
-  while (file.available())
-  {
-    Serial.write(file.read());
-  }
-  file.close();
-}
-
-void writeFile(fs::FS &fs, const char *path, const char *message)
-{
-  Serial.printf("Writing file: %s\n", path);
-
-  File file = fs.open(path, FILE_WRITE);
-  if (!file)
-  {
-    Serial.println("Failed to open file for writing");
-    return;
-  }
-  if (file.print(message))
-  {
-    Serial.println("File written");
-  }
-  else
-  {
-    Serial.println("Write failed");
-  }
-  file.close();
-}
-
-void appendFile(fs::FS &fs, const char *path, const char *message)
-{
-  Serial.printf("Appending to file: %s\n", path);
-
-  File file = fs.open(path, FILE_APPEND);
-  if (!file)
-  {
-    Serial.println("Failed to open file for appending");
-    return;
-  }
-  if (file.print(message))
-  {
-    Serial.println("Message appended");
-  }
-  else
-  {
-    Serial.println("Append failed");
-  }
-  file.close();
-}
-
-void renameFile(fs::FS &fs, const char *path1, const char *path2)
-{
-  Serial.printf("Renaming file %s to %s\n", path1, path2);
-  if (fs.rename(path1, path2))
-  {
-    Serial.println("File renamed");
-  }
-  else
-  {
-    Serial.println("Rename failed");
-  }
-}
-
-void deleteFile(fs::FS &fs, const char *path)
-{
-  Serial.printf("Deleting file: %s\n", path);
-  if (fs.remove(path))
-  {
-    Serial.println("File deleted");
-  }
-  else
-  {
-    Serial.println("Delete failed");
-  }
-}
-
-void testFileIO(fs::FS &fs, const char *path)
-{
-  File file = fs.open(path);
-  static uint8_t buf[512];
-  size_t len = 0;
-  uint32_t start = millis();
-  uint32_t end = start;
-  if (file)
-  {
-    len = file.size();
-    size_t flen = len;
-    start = millis();
-    while (len)
-    {
-      size_t toRead = len;
-      if (toRead > 512)
-      {
-        toRead = 512;
-      }
-      file.read(buf, toRead);
-      len -= toRead;
-    }
-    end = millis() - start;
-    Serial.printf("%u bytes read for %u ms\n", flen, end);
-    file.close();
-  }
-  else
-  {
-    Serial.println("Failed to open file for reading");
-  }
-
-  file = fs.open(path, FILE_WRITE);
-  if (!file)
-  {
-    Serial.println("Failed to open file for writing");
-    return;
-  }
-
-  size_t i;
-  start = millis();
-  for (i = 0; i < 2048; i++)
-  {
-    file.write(buf, 512);
-  }
-  end = millis() - start;
-  Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
-  file.close();
-}
+/*
+canbus pinout
+tx = GPIO_NUM_14;
+rx = GPIO_NUM_27;
+*/
 
 void canbus_setup()
 {
   CAN_cfg.speed = CAN_SPEED_250KBPS;
-  CAN_cfg.tx_pin_id = GPIO_NUM_5;
-  CAN_cfg.rx_pin_id = GPIO_NUM_4;
+  CAN_cfg.tx_pin_id = GPIO_NUM_14;
+  CAN_cfg.rx_pin_id = GPIO_NUM_27;
   CAN_cfg.rx_queue = xQueueCreate(rx_queue_size, sizeof(CAN_frame_t));
   // Init CAN Module
   ESP32Can.CANInit();
@@ -273,7 +75,22 @@ void setup()
   uint64_t cardSize = SD.cardSize() / (1024 * 1024);
   Serial.printf("SD Card Size: %lluMB\n", cardSize);
 
-  listDir(SD, "/", 0);
+  int count_files = listDir(SD, "/", 0);
+  count_files++;
+  char current_filename[128];
+  sprintf(current_filename, "/log%04d.log", count_files);
+  file_log = SD.open(current_filename, FILE_APPEND);
+  if (!file_log)
+  {
+    Serial.printf("Failed to open %s file for appending\n", (char*)current_filename);
+    return;
+  }
+  else
+  {
+    Serial.printf("opened %s file for appending\n", (char*)current_filename);
+    file_log.flush();
+  }
+
   // createDir(SD, "/mydir");
   // listDir(SD, "/", 0);
   // removeDir(SD, "/mydir");
@@ -291,4 +108,38 @@ void setup()
 
 void loop()
 {
+  CAN_frame_t rx_frame;
+
+  // Receive next CAN frame from queue
+  if (xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS) == pdTRUE)
+  {
+
+    // if (rx_frame.FIR.B.FF == CAN_frame_std)
+    // {
+    //   printf("New standard frame");
+    // }
+    // else
+    // {
+    //   printf("New extended frame");
+    // }
+
+    if (rx_frame.FIR.B.RTR == CAN_RTR)
+    {
+      printf(" RTR from 0x%08X, DLC %d\r\n", rx_frame.MsgID, rx_frame.FIR.B.DLC);
+      file_log.printf("%d 0x%08X %d RTR\n", millis(), rx_frame.MsgID, rx_frame.FIR.B.DLC);
+    }
+    else
+    {
+      printf(" from 0x%08X, DLC %d, Data ", rx_frame.MsgID, rx_frame.FIR.B.DLC);
+      file_log.printf("%d 0x%08X %d ", millis(), rx_frame.MsgID, rx_frame.FIR.B.DLC);
+      for (int i = 0; i < rx_frame.FIR.B.DLC; i++)
+      {
+        printf("0x%02X ", rx_frame.data.u8[i]);
+        file_log.printf("0x%02X ", rx_frame.data.u8[i]);
+      }
+      printf("\n");
+      file_log.printf("\n");
+    }
+    file_log.flush();
+  }
 }
